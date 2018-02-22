e4de7130b5adbc61c0877622739a20feab77e286
Fernando Crespo <fernando82@gmail.com>
2018-02-21 23:15:23 -0300

    Enable the markdown table formatter to active language

--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,15 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
     var commandFormat = vscode.commands.registerTextEditorCommand("markdown-table-formatter.format", (editor, edit) => {
-
         vscode.window.showInformationMessage('Tables formatted!');
     });
 
     var commandEnable = vscode.commands.registerTextEditorCommand("markdown-table-formatter.enableForCurrentScope", (editor, edit) => {
-
-        var config = vscode.workspace.getConfiguration('martdown-table-formatter');
-
-        var array = config.get("markdownGrammarScopes");
-
-        //array.push(editor.document.languageId);
-
-        //config.update
-
-        vscode.window.showInformationMessage(`Markdown table formatter enabled for ${editor.document.languageId} language scope!`);
+        var config = vscode.workspace.getConfiguration('markdown-table-formatter');
+        var array : [String] = config.markdownGrammarScopes;
+        array.push(editor.document.languageId);
+        config.update("markdownGrammarScopes", array, true);
+        vscode.window.showInformationMessage(`Markdown table formatter enabled for '${editor.document.languageId}' language scope!`);
     });
 
     context.subscriptions.push(commandFormat);

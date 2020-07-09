7361f1d4a72be57da08bfc7d14b1acf0c408884a
Fernando Crespo <fernando82@gmail.com>
2020-07-09 20:53:46 -0300

    Add automation to create a release tag and a packaged version of the extension

---
+++ .github/actions/vsce-package/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+# Install dependencies from your project
+npm i
+
+# Packages the Visual Studio Code extension
+vsce package
+
+# Exports the name to the next step
+name=`ls *.vsix`
+echo "::set-output name=package_name::$name"
